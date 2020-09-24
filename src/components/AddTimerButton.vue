c8028cc9eccf1d5e6c2d39dfd294cec6d1e050a8
Daniil <daniil.belobragin.work@gmail.com>
2020-09-24 05:33:13 +0300

    task completed

---
+++ src/components/AddTimerButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="timer">
+    <custom-button :path="require('../assets/image/add.svg')" image-alt="add" :click-function="clickFunction" />
+  </div>
+</template>
+
+<script>
+import CustomButton from "@/components/CustomButton"
+
+export default {
+  name: "AddTimerButton",
+
+  components: {CustomButton},
+
+  props: {
+    clickFunction: {
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "../assets/style/style";
+</style>
